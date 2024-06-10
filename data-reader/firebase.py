a9a3c979cda2082689f8ed44fea92de6ea21f635
uruthia <maria.elena.zuliani@gmail.com>
2024-06-10 21:44:15 +0200

    fix regular expression filter

--- data-reader/firebase.py
+++ data-reader/firebase.py
@@ -10,7 +10,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def store_coordinate(data):
-    if ((data.length() == 5) and (re.match(r'^\d{2}\.\d{6}$', data[1])) and (re.match(r'^\d{2}\.\d{6}$', data[2])) and (re.match(r'^\d{2}\.\d{2}$', data[3])) and (re.match(r'^\d{2}\.\d{2}$', data[4]))):
+    if ((len(data) == 5) and (re.match(r'^\d{2}\.\d{6}$', data[1])) and (re.match(r'^\d{2}\.\d{6}$', data[2])) ):
         doc_ref = db.collection('coordinates').document()
         doc_ref.set({
             'latitude': data[1],
@@ -22,7 +22,7 @@ def store_coordinate(data):
     
 def store_anomalie(data):
     print(data)
-    if ((data.length() == 4) and (re.match(r'^\d{3}', data[1])) and (re.match(r'^\d{2}\.\d{6}$', data[2])) and (re.match(r'^\d{2}\.\d{6}$', data[3]))): #controlla che ci siano almento 4 valori nel pacchetto inviato dal sender
+    if ((len(data) == 4) and (re.match(r'^\d{3}', data[1])) and (re.match(r'^\d{2}\.\d{6}$', data[2])) and (re.match(r'^\d{2}\.\d{6}$', data[3]))): #controlla che ci siano almento 4 valori nel pacchetto inviato dal sender
         doc_ref = db.collection('anomalies').document()
         doc_ref.set({
             'type': data[0],
