92d061a3fd5d09e6e988f58ee1572f4cdc492d3e
uruthia <maria.elena.zuliani@gmail.com>
2024-06-10 10:20:48 +0200

    add filter by regular expression

--- data-reader/firebase.py
+++ data-reader/firebase.py
@@ -1,3 +1,4 @@
+import re
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -9,22 +10,24 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def store_coordinate(data):
-    doc_ref = db.collection('coordinates').document()
-    doc_ref.set({
-        'latitude': data[1],
-        'longitude': data[2],
-        'altitude': data[3],
-        'speed': data[4],
-        'timestamp': firestore.SERVER_TIMESTAMP
-    })
+    if ((data.length() == 5) and (re.match(r'^\d{2}\.\d{6}$', data[1])) and (re.match(r'^\d{2}\.\d{6}$', data[2])) and (re.match(r'^\d{2}\.\d{2}$', data[3])) and (re.match(r'^\d{2}\.\d{2}$', data[4]))):
+        doc_ref = db.collection('coordinates').document()
+        doc_ref.set({
+            'latitude': data[1],
+            'longitude': data[2],
+            'altitude': data[3],
+            'speed': data[4],
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
     
 def store_anomalie(data):
     print(data)
-    doc_ref = db.collection('anomalies').document()
-    doc_ref.set({
-        'type': data[0],
-        'value': data[1],
-        'latitude': data[2],
-        'longitude': data[3],
-        'timestamp': firestore.SERVER_TIMESTAMP
-    })
+    if ((data.length() == 4) and (re.match(r'^\d{3}', data[1])) and (re.match(r'^\d{2}\.\d{6}$', data[2])) and (re.match(r'^\d{2}\.\d{6}$', data[3]))): #controlla che ci siano almento 4 valori nel pacchetto inviato dal sender
+        doc_ref = db.collection('anomalies').document()
+        doc_ref.set({
+            'type': data[0],
+            'value': data[1],
+            'latitude': data[2],
+            'longitude': data[3],
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
